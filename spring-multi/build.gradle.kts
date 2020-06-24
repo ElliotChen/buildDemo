2c0121338837cb803e8f27a3ef5d8420079c536b
Elliot Chen <elliot.chen@elliot.tw>
2020-06-24 10:50:09 +0800

    revise kts

--- spring-multi/build.gradle.kts
+++ spring-multi/build.gradle.kts
@@ -1,9 +1,10 @@
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
     id("org.springframework.boot") version "2.3.1.RELEASE" apply false
-    id("io.spring.dependency-management") version "1.0.9.RELEASE" apply false
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
     kotlin("jvm") version "1.3.72" apply false
     kotlin("plugin.spring") version "1.3.72" apply false
 }
@@ -17,14 +18,13 @@ allprojects {
     }
 }
 
+
+
 subprojects {
 
     println("Enabling Java And Kotlin in project ${project.name}...")
     apply(plugin = "java")
     apply(plugin = "kotlin")
-
-    println("Enabling Spring Boot plugin in project ${project.name}...")
-    apply(plugin = "org.springframework.boot")
 
     tasks.withType<KotlinCompile> {
         println("Configuring KotlinCompile  $name in project ${project.name}...")
@@ -36,20 +36,18 @@ subprojects {
 
     println("Enabling Spring Boot Dependency Management in project ${project.name}...")
     apply(plugin = "io.spring.dependency-management")
-    /*
-    configure<DependencyManagementExtension> {
+
+    the<DependencyManagementExtension>().apply {
         imports {
             mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
         }
     }
-    */
+
     tasks.withType<Test> {
         useJUnitPlatform()
     }
 
     dependencies {
-        implementation("org.springframework.boot:spring-boot-starter-actuator")
-        implementation("org.springframework.boot:spring-boot-starter-web")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -61,6 +59,6 @@ subprojects {
     }
 }
 
-tasks.forEach { // 禁止根项目构建
+tasks.forEach {
     it.enabled = false
 }
